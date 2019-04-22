842298d722006bae735cd630766fb2791ae9ab08
Rohit Pathak <rohitpathak@Vinitas-MacBook-Pro.local>
2019-04-22 14:57:32 -0400

    created initial main file

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import datetime
+import platform
+
+print('Current date :{}'.format(datetime.datetime.now()))
+print('Current python path :{}'.format(sys.executable))
+print('{} {}: {}'.format(platform.system(),platform.release(),platform.version()))
+print('Hello World')
