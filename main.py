702b1e0121eb69eec641797dda19ad0bef0d61cd
Rohit Pathak <pathakrohit08@gmail.com>
2019-04-22 14:59:36 -0400

    Update main.py
    checking if the changes flow from git to desktop

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import datetime
 import platform
 
+# trying to send changes from git to vs code
 print('Current date :{}'.format(datetime.datetime.now()))
 print('Current python path :{}'.format(sys.executable))
 print('{} {}: {}'.format(platform.system(),platform.release(),platform.version()))
